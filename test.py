4169950737a0a61ca4bfae79674a9001441e2df4
sempau67 <166898785+sempau67@users.noreply.github.com>
2025-07-10 22:46:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-return render_template('index.html', button_html=button_html)
 
-
-
-
- <section class="main__skills skills" id="skills">
-        <form action="/" method="POST">
-          <ul class="skills__list skills-list">
-            <li class="skills-list__skill skill">
-              <img
-                class="skill__img"
-                src="../static/img/python.png"
-                alt="python"
-                width="150"
-                height="150"
-              />
